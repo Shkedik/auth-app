f871997e79a18e7be3c29da9cc05af7dcb813258
Alexandra <mirinda-001@list.ru>
2021-02-15 19:11:12 +0300

    new component logo some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -10,20 +11,25 @@ const appRoutes: Routes = [
     path        : 'app', 
     loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule),
   },
+  {
+    path        : 'apps', 
+    loadChildren: () => import('./apps/apps.module').then(m => m.AppsModule),
+  },
   {   
     path: "**",
-    redirectTo:"/app/auth/login",
+    redirectTo:"/app/auth/register",
     pathMatch: "full"
   }
 ]
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     RouterModule.forRoot(appRoutes)
 
   ],
